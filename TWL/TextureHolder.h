47cd8b5ae271967b40ce958ce786acf10475fa6d
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-09-17 10:39:47 +0800

    Project Finished

--- TWL/TextureHolder.h
+++ TWL/TextureHolder.h
@@ -1,22 +1,25 @@
 #pragma once
-
 #ifndef TEXTURE_HOLDER_H
 #define TEXTURE_HOLDER_H
 
 #include <SFML/Graphics.hpp>
 #include <map>
 
-class TextureHolder {
+class TextureHolder
+{
 private:
-	// STL map container which holds related pairs of "String" and "Texture"
+	// A map container from the STL,
+	// that holds related pairs of String and Texture
 	std::map<std::string, sf::Texture> m_Textures;
 
-	// pointer of the same type as the class itself
+	// A pointer of the same type as the class itself
+	// the one and only instance
 	static TextureHolder* m_s_Instance;
 
 public:
 	TextureHolder();
 	static sf::Texture& GetTexture(std::string const& filename);
+
 };
 
 #endif
